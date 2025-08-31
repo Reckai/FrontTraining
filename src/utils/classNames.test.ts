338020d25a6306f48b795c1d65d705c9b8e05daf
Reckai <casa101565@gmail.com>
2025-08-31 20:54:35 +0300

    feat: add tests to cn function

--- src/utils/classNames.test.ts
+++ src/utils/classNames.test.ts
@@ -2,7 +2,11 @@ import { describe, expect, it } from 'vitest'
 import { cn } from './classNames.ts'
 
 describe('classNames', () => {
-  it('classNames', () => {
+  it('with only one param', () => {
     expect(cn('className')).toBe('className')
+  })
+
+  it('with several params', () => {
+    expect(cn('className', 'asd')).toBe('className asd')
   })
 })
