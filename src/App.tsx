9423a723ab4cfae197c40d9433df26d0b0fa2ffb
Reckai <casa101565@gmail.com>
2025-08-31 15:44:29 +0300

    feat: add internationalization

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
+import { Suspense } from 'react'
+import { useTranslation } from 'react-i18next'
 import Navbar from '@/modules/Navbar/Navbar.tsx'
 import Sidebar from '@/modules/Sidebar/Sidebar.tsx'
 import AppRouter from './providers/router/AppRouter'
 
+function MyComponent() {
+  const { t, i18n } = useTranslation('common')
+
+  return (
+    <h1>
+      {t('s ')}
+      <button onClick={() => i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')}>
+        change localization
+      </button>
+    </h1>
+  )
+}
 function App() {
   return (
     <div className="text-primary h-screen flex-col flex ">
-      <Navbar />
-      <div className="flex grow">
-        <Sidebar />
-        <div className="flex grow p-sm">
-          <AppRouter />
+      <Suspense>
+        <Navbar />
+        <div className="flex grow">
+          <Sidebar />
+          <div className="flex grow p-sm">
+            <AppRouter />
+            <MyComponent />
+          </div>
         </div>
-      </div>
+      </Suspense>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import App from './App.tsx'
+
 import ThemeProvider from './theme/ThemeProvider.tsx'
+
+import './providers/Localization/i18n.ts'
+
 import './styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
