dd550705284c4d6f3cb95911edb3796f96fb5f3a
Reckai <casa101565@gmail.com>
2025-08-31 16:11:26 +0300

    feat: add plugin for eslint, it gives us error where we dont use intenationalization

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,18 @@
 import antfu from '@antfu/eslint-config'
+import i18next from 'eslint-plugin-i18next'
 
-export default antfu()
+export default antfu({}, {
+  plugins: {
+    i18next,
+  },
+  rules: {
+    'i18next/no-literal-string': ['error', {
+      // Customize options here
+      markupOnly: true, // Only check JSX markup (default behavior)
+      // You can add more options like:
+      // ignoreCallee: ['yourFunction'],
+      // ignoreAttribute: ['data-testid', 'className'],
+      // etc.
+    }],
+  },
+})
