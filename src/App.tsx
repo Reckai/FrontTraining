8f45ff8c9547b74dd5a07e22c28046a2dbf04de4
Reckai <casa101565@gmail.com>
2025-09-09 21:29:58 +0300

    refactor: delete component language toggle from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
 import { Suspense } from 'react'
-import { useTranslation } from 'react-i18next'
 import Navbar from '@/modules/Navbar/Navbar.tsx'
 import Sidebar from '@/modules/Sidebar/Sidebar.tsx'
 import AppRouter from './providers/router/AppRouter'
 
-function MyComponent() {
-  const { t, i18n } = useTranslation('common')
-
-  return (
-    <h1>
-      {t('s ')}
-      <button onClick={() => i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')}>
-        change localization
-      </button>
-    </h1>
-  )
-}
 function App() {
   return (
     <div className="text-primary h-screen flex-col flex ">
@@ -25,7 +12,7 @@ function App() {
           <Sidebar />
           <div className="flex grow p-sm">
             <AppRouter />
-            <MyComponent />
+
           </div>
         </div>
       </Suspense>
