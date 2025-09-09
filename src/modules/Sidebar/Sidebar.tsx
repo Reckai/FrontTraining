f007ef42753b6abf9dedc2183e774e4b6a3e9cfe
Reckai <casa101565@gmail.com>
2025-09-09 21:29:20 +0300

    feat: add language toggle to sidebar

--- src/modules/Sidebar/Sidebar.tsx
+++ src/modules/Sidebar/Sidebar.tsx
@@ -1,31 +1,46 @@
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 import React from 'react'
+import LanguageToggle from '@/modules/Navbar/LanguageToggle/LanguageToggle.tsx'
+import Button from '@/modules/shared/Button/Button.tsx'
 import { useToggle } from '@/modules/shared/hooks/useToggle/useToggle.tsx'
-import { useTheme } from '@/theme/hooks/useTheme.ts'
 import { cn } from '@/utils/classNames.ts'
 
 interface SidebarProps extends React.ComponentProps<'div'> {}
 
 function Sidebar({ className }: SidebarProps) {
   const [open, toggle] = useToggle(false)
-  const { theme } = useTheme()
+
   return (
     <motion.div
-      initial={{ flexBasis: '80px' }} // 1/6
-      animate={{ flexBasis: open ? '300px' : '80px' }} // 2/6 vs 1/6
+      initial={{ flexBasis: '80px' }}
+      animate={{ flexBasis: open ? '300px' : '80px' }}
       transition={{ duration: 0.3, ease: 'easeInOut' }}
       className={cn(
         'flex border-border border-[1px] rounded-r-xl items-start p-sm bg-surface min-w-min',
         className,
       )}
     >
-      <button
-        className={cn('cursor-pointer bg-primary  rounded-md p-sm', theme === 'dark' ? 'text-background' : 'text-white')}
-        onClick={toggle}
-      >
-        {open ? 'Open' : 'Closed'}
-      </button>
+      <div className="self-end gap-2 flex flex-col justify-items-center">
+        <AnimatePresence>
+          {open && (
+            <motion.div
+              initial={{ opacity: 0, scale: 0.8 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.8 }}
+              transition={{
+                duration: 0.3,
+                ease: 'easeOut',
+              }}
+            >
+              <LanguageToggle />
+            </motion.div>
+          )}
+        </AnimatePresence>
 
+        <Button onClick={toggle}>
+          {open ? 'Close' : 'Open'}
+        </Button>
+      </div>
     </motion.div>
   )
 }
