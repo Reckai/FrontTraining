9226ce0cd866070f3bcf245c1ba80e87eed84b52
Reckai <casa101565@gmail.com>
2025-09-10 00:43:49 +0300

    feat: add fallback for pages

--- src/providers/router/AppRouter.tsx
+++ src/providers/router/AppRouter.tsx
@@ -1,16 +1,25 @@
+import { Suspense } from 'react'
 import { Route, Routes } from 'react-router'
+import PageLoader from '@/modules/PageLoader/PageLoader.tsx'
 import { routeConfig } from './config'
 
 function AppRouter() {
   return (
-
     <Routes>
-      {
-        routeConfig.map(({ path, element }) => <Route key={path} path={path} element={element} />)
-      }
+      {routeConfig.map(({ element, path }) => (
+        <Route
+          key={path}
+          path={path}
+          element={(
+            <Suspense fallback={<PageLoader />}>
+              <div className="w-full h-full">
+                {element}
+              </div>
+            </Suspense>
+          )}
+        />
+      ))}
     </Routes>
-
   )
 }
-
 export default AppRouter
