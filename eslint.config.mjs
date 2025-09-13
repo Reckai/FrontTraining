142d615d07454ded8fcbf7447fc21298952647a5
Reckai <casa101565@gmail.com>
2025-09-13 17:06:20 +0300

    fix: add eslint ignore to .github

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,24 @@
 import antfu from '@antfu/eslint-config'
 import i18next from 'eslint-plugin-i18next'
 
-export default antfu({}, {
-  plugins: {
-    i18next,
-  },
-  ignores: ['.github/**/*'],
-  rules: {
-    'node/prefer-global/process': ['error', 'always'],
-    'i18next/no-literal-string': ['error', {
-      markupOnly: true, // Only check JSX markup (default behavior)
-
-    }],
-  },
-}, {
-  files: ['**/*.test.*', '**/*.spec.*', '**/__tests__/**/*', '**/tests/**/*'],
-  rules: {
-    'i18next/no-literal-string': 'off',
-  },
-})
+export default [
+    {
+        ignores: ['.github/**/*'],
+    },
+    ...antfu({}, {
+        plugins: {
+            i18next,
+        },
+        rules: {
+            'node/prefer-global/process': ['error', 'always'],
+            'i18next/no-literal-string': ['error', {
+                markupOnly: true, // Only check JSX markup (default behavior)
+            }],
+        },
+    }, {
+        files: ['**/*.test.*', '**/*.spec.*', '**/__tests__/**/*', '**/tests/**/*'],
+        rules: {
+            'i18next/no-literal-string': 'off',
+        },
+    }),
+]
